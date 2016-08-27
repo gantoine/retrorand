b6e6c90fe8721cbcf23c72ddb7c95dca5be8790c
Georges-Antoine Assi <georges.antoine.assi@gmail.com>
2016-08-26 20:22:52 -0400

    Yo node generation

---
+++ test/index.js
@@ -0,0 +1,8 @@
+import assert from 'assert';
+import randomGameApp from '../lib';
+
+describe('random-game-app', function () {
+  it('should have unit test!', function () {
+    assert(false, 'we expected this package author to add actual unit tests.');
+  });
+});
